073564b94ce99a742267e31daeb40c3c97d3d331
Lukasz <kobiel6@wp.pl>
2019-03-12 15:22:34 +0000

    ???

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://archiveagain.github.io/CycleWorld/success.html');
+    header('Location: /success.html');
   } ?>
