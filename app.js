a2d40b1c1457411173be2bffd480d911aa3f64c8
Lukasz <kobiel6@wp.pl>
2019-03-12 00:42:58 +0000

    1 commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let text = ["Cycle The World", "Viajar El Mundo ", "Zwiedzaj Świat"];
+let counter = 0;
+
+const h1 = document.getElementById("changeText");
+let interval = setInterval(change, 3000);
+
+function change() {
+  h1.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+  }
+}
+
+// Smooth Scroll
+$("#navbar a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 60
+      },
+      800
+    );
+  }
+});
